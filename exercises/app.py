56635661ae98b15af2451d287d84fd46f2a6812b
yousef19-meet <yousef19@meet.mit.edu>
2018-08-07 08:24:52 -0700

    flask

--- exercises/app.py
+++ exercises/app.py
@@ -1,9 +1,27 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-
+#part 1
 @app.route('/')
 def home_page():
-    pass
+    x= "get off my lawn bish... but /sport and get outta here"
+    print(x)
+    return x
+#############
+#part 2
+@app.route('/sport') 
+def favorite_sport():
+    return render_template ("index.html")
+#############
+#part 3
+@app.route('/green')
+def hello_world():
+    return render_template(
+"index.html")
 
+
+
+
+
+#############
 if __name__ == '__main__':
    app.run(debug = True)
